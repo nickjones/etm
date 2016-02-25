b948b1e1e0a6c18928015d55043fd53fe67650c0
Nick Jones <darellik@gmail.com>
2016-02-25 08:05:39 -0600

    Fix temp file generation.

--- etf/decode.go
+++ etf/decode.go
@@ -16,12 +16,14 @@ const secondLastByte = lastByteRecord - 1
 
 // NewDecoder stream parses a raw Embedded Trace FIFO format binary dump into
 // only ETM trace for a specific stream.
-func NewDecoder(in io.Reader) (fn map[uint64]string, e error) {
+func NewDecoder(in io.Reader) (map[uint64]string, error) {
 	log.Debugln("Creating new ETF Decoder")
 
 	// Map trace IDs to file pointers.  It'll be up to the caller to unlink
 	// the unnecessary files.
-	var files map[uint64]*os.File
+	files := make(map[uint64]*os.File)
+	// Filename map (will actually be returned to the caller)
+	fn := make(map[uint64]string)
 
 	// Defer closing each file we create. Caller must open the one of interest.
 	defer func() {
@@ -51,7 +53,7 @@ func NewDecoder(in io.Reader) (fn map[uint64]string, e error) {
 			aux := buf[lastByteRecord] >> (i / 2) & 1
 			log.Debugf("buf[%0d]=%#v buf[%0d]=%#v buf[15]=%#v aux=%#v", i, buf[i], i+1, buf[i+1], buf[lastByteRecord], aux)
 			if buf[i]&1 == 0 { // Data mode
-				fn[curID] = makeTempFile(files[curID])
+				fn[curID] = makeTempFile(files, curID)
 				data := []byte{(buf[i] & 0xfe) | ((buf[lastByteRecord] >> (i / 2)) & 1)}
 
 				files[curID].Write(data)
@@ -64,13 +66,13 @@ func NewDecoder(in io.Reader) (fn map[uint64]string, e error) {
 				curID, _ = binary.Uvarint([]byte{byteID})
 				log.Debugf("ID update %d -> %d", oldID, curID)
 
-				fn[curID] = makeTempFile(files[curID])
+				fn[curID] = makeTempFile(files, curID)
 				files[curID].Write([]byte{buf[i+1]})
 
 				log.Debugf("New ID immediately, writing byte: %#v ID %d", buf[i+1], curID)
 			} else if buf[i]&1 == 1 && aux == 1 { // Update with new ID
 				oldID := curID
-				fn[curID] = makeTempFile(files[curID])
+				fn[curID] = makeTempFile(files, curID)
 				files[curID].Write([]byte{buf[i+1]})
 
 				log.Debugf("New ID, writing byte: %#v ID %d", buf[i+1], curID)
@@ -87,7 +89,7 @@ func NewDecoder(in io.Reader) (fn map[uint64]string, e error) {
 			byteID := (buf[secondLastByte] >> 1) & 0x7f
 			curID, _ = binary.Uvarint([]byte{byteID})
 			log.Debugf("ID update %d -> %d", oldID, curID)
-			fn[curID] = makeTempFile(files[curID])
+			fn[curID] = makeTempFile(files, curID)
 		} else {
 			data := []byte{(buf[secondLastByte] & 0xfe) | (buf[lastByteRecord]>>7)&1}
 			log.Debugf("Writing byte 14: %#v ID %d", data[0], curID)
@@ -97,13 +99,13 @@ func NewDecoder(in io.Reader) (fn map[uint64]string, e error) {
 	return fn, nil
 }
 
-func makeTempFile(f *os.File) string {
-	if f == nil {
+func makeTempFile(files map[uint64]*os.File, id uint64) string {
+	if files[id] == nil {
 		var err error
-		f, err = ioutil.TempFile("", "etm-etf")
+		files[id], err = ioutil.TempFile("", fmt.Sprintf("etm-etf%d-", id))
 		if err != nil {
 			log.Errorf("Failed to create a temp file: %q", err)
 		}
 	}
-	return f.Name()
+	return files[id].Name()
 }
