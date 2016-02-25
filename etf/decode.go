7e6e50c675f9ead6026ab43bfa90f559e39450e5
Nick Jones <darellik@gmail.com>
2016-02-24 20:36:30 -0600

    Second gen ETF support with serious clean up.

---
+++ etf/decode.go
@@ -0,0 +1,109 @@
+package etf
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+const recordLen = 16
+const lastByteRecord = recordLen - 1
+const secondLastByte = lastByteRecord - 1
+
+// NewDecoder stream parses a raw Embedded Trace FIFO format binary dump into
+// only ETM trace for a specific stream.
+func NewDecoder(in io.Reader) (fn map[uint64]string, e error) {
+	log.Debugln("Creating new ETF Decoder")
+
+	// Map trace IDs to file pointers.  It'll be up to the caller to unlink
+	// the unnecessary files.
+	var files map[uint64]*os.File
+
+	// Defer closing each file we create. Caller must open the one of interest.
+	defer func() {
+		for i := range files {
+			files[i].Close()
+		}
+	}()
+
+	buf := make([]byte, recordLen)
+	var curID uint64 // Current ID in the trace (may not be ours...)
+	var i uint
+
+	curID = 0
+
+	for {
+		n, err := io.ReadAtLeast(in, buf, recordLen)
+
+		if err == io.ErrUnexpectedEOF || err == io.ErrShortBuffer {
+			return nil, fmt.Errorf("Error received reading input file: %q", err)
+		} else if n == 0 {
+			break
+		}
+
+		log.Debugf("Read %d bytes, current ID: %d", n, curID)
+
+		for i = 0; i < secondLastByte; i += 2 {
+			aux := buf[lastByteRecord] >> (i / 2) & 1
+			log.Debugf("buf[%0d]=%#v buf[%0d]=%#v buf[15]=%#v aux=%#v", i, buf[i], i+1, buf[i+1], buf[lastByteRecord], aux)
+			if buf[i]&1 == 0 { // Data mode
+				fn[curID] = makeTempFile(files[curID])
+				data := []byte{(buf[i] & 0xfe) | ((buf[lastByteRecord] >> (i / 2)) & 1)}
+
+				files[curID].Write(data)
+				files[curID].Write([]byte{buf[i+1]})
+
+				log.Debugf("Data mode, writing byte: %#v and %#v ID %d", data[0], buf[i+1], curID)
+			} else if buf[i]&1 == 1 && aux == 0 { // bit[0] set, new ID immediately
+				oldID := curID
+				byteID := (buf[i] >> 1) & 0x7f
+				curID, _ = binary.Uvarint([]byte{byteID})
+				log.Debugf("ID update %d -> %d", oldID, curID)
+
+				fn[curID] = makeTempFile(files[curID])
+				files[curID].Write([]byte{buf[i+1]})
+
+				log.Debugf("New ID immediately, writing byte: %#v ID %d", buf[i+1], curID)
+			} else if buf[i]&1 == 1 && aux == 1 { // Update with new ID
+				oldID := curID
+				fn[curID] = makeTempFile(files[curID])
+				files[curID].Write([]byte{buf[i+1]})
+
+				log.Debugf("New ID, writing byte: %#v ID %d", buf[i+1], curID)
+				byteID := (buf[i] >> 1) & 0x7f
+				curID, _ = binary.Uvarint([]byte{byteID})
+
+				log.Debugf("ID update %d -> %d", oldID, curID)
+			} else {
+				log.Errorln("Not sure what to do with this byte[%d]=%#v byte[%d]=%#v", i, buf[i], lastByteRecord, buf[lastByteRecord])
+			}
+		}
+		if buf[secondLastByte]&1 == 1 { // Update ID
+			oldID := curID
+			byteID := (buf[secondLastByte] >> 1) & 0x7f
+			curID, _ = binary.Uvarint([]byte{byteID})
+			log.Debugf("ID update %d -> %d", oldID, curID)
+			fn[curID] = makeTempFile(files[curID])
+		} else {
+			data := []byte{(buf[secondLastByte] & 0xfe) | (buf[lastByteRecord]>>7)&1}
+			log.Debugf("Writing byte 14: %#v ID %d", data[0], curID)
+			files[curID].Write(data)
+		}
+	}
+	return fn, nil
+}
+
+func makeTempFile(f *os.File) string {
+	if f == nil {
+		var err error
+		f, err = ioutil.TempFile("", "etm-etf")
+		if err != nil {
+			log.Errorf("Failed to create a temp file: %q", err)
+		}
+	}
+	return f.Name()
+}
--- etm_decoder.go
+++ etm_decoder.go
@@ -2,26 +2,28 @@ package main
 
 import (
 	"bufio"
-	"encoding/binary"
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
-	"sarc/etm-decode/trace_packets_v4"
 
 	log "github.com/Sirupsen/logrus"
+	etf "github.com/nickjones/etm/etf"
+	pkts "github.com/nickjones/etm/tracepkts"
 )
 
+// Build semantic version
 var VERSION string
+
+// Date of build
 var BUILD_DATE string
 
 var (
 	debug         = flag.Bool("debug", false, "Debug logging.")
 	etfMode       = flag.Bool("etf", false, "Input file is a binary ETF trace dump.")
 	noEtfSync     = flag.Bool("noetfsync", true, "Input ETF binary lacks a frame sync.")
-	etfEtmId      = flag.Int("id", 0, "Trace ID for ETM traffic to parse in ETF mode.")
-	dbgDisIdCheck = flag.Bool("disidchk", false, "Disable ETF trace ID checks.")
+	etfEtmID      = flag.Int("id", 0, "Trace ID for ETM traffic to parse in ETF mode.")
+	dbgDisIDCheck = flag.Bool("disidchk", false, "Disable ETF trace ID checks.")
 	keepTmp       = flag.Bool("keeptmpbin", false, "Keep temporary ETF->ETM file.")
 )
 
@@ -49,7 +51,6 @@ func main() {
 
 	fmt.Println("Filename:", filename)
 
-	var tmp *os.File
 	var err error
 
 	file, err := os.Open(filename)
@@ -62,20 +63,33 @@ func main() {
 	if *etfMode {
 		log.Println("Parsing input as ETF trace storage format.")
 		// Input file needs to be parsed into raw ETM trace first
-		tmp, err = ioutil.TempFile("", "etmv4-decoder")
+		traceFiles, err := etf.NewDecoder(file)
 
-		parseEtfToEtm(*etfEtmId, file, tmp)
-
-		_, err = tmp.Seek(0, 0)
-		if err != nil {
-			fmt.Errorf("Error trying to seek backwards on temp file: %q", err)
+		// Open the temp file of interest to the user
+		traceID := uint64(*etfEtmID)
+		if len(traceFiles[traceID]) > 0 {
+			file, err = os.Open(traceFiles[traceID])
+			if err != nil {
+				log.Errorf("Error opening temp file from ETF parsing: %q", err)
+			}
+		} else {
+			log.Errorf("Unable to find ETF trace ID %d", *etfEtmID)
 		}
-		file = tmp
+
+		defer file.Close()
+		// ETF parsing creates temp files for each ID, remove them all (eventually)
+		if !*keepTmp {
+			defer func() {
+				for i := range traceFiles {
+					os.Remove(traceFiles[i])
+				}
+			}()
+		}
 	}
 
 	log.Debugln("Synchronizing trace stream.  Looking for Async")
 	// Sync trace stream; search for consecutive bytes of 00 (at least 11) followed by 80
-	async_byte_count := 0
+	asyncByteCnt := 0
 	for {
 		b := make([]byte, 1)
 		_, err := file.Read(b)
@@ -87,22 +101,22 @@ func main() {
 		if *debug {
 			log.Printf("Current byte: %x\n", b)
 		}
-		if async_byte_count < 11 && b[0] == 0x00 {
-			async_byte_count++
-		} else if async_byte_count == 11 && b[0] == 0x80 {
+		if asyncByteCnt < 11 && b[0] == 0x00 {
+			asyncByteCnt++
+		} else if asyncByteCnt == 11 && b[0] == 0x80 {
 			// Trace unit synchronized
 			break
 		} else {
-			async_byte_count = 0
+			asyncByteCnt = 0
 		}
 	}
-	trace_start_pos, err := file.Seek(0, os.SEEK_CUR)
+	traceStartPos, err := file.Seek(0, os.SEEK_CUR)
 	if *debug {
-		log.Println("Trace unit synchronized at fpos ", trace_start_pos)
+		log.Println("Trace unit synchronized at fpos ", traceStartPos)
 	}
 
 	// Seek backwards to the beginning of ASYNC
-	_, err = file.Seek(trace_start_pos-12, os.SEEK_SET)
+	_, err = file.Seek(traceStartPos-12, os.SEEK_SET)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -116,83 +130,11 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		pkt := etmv4_trace_packets.DecodePacket(header, input)
+		pkt := pkts.DecodePacket(header, input)
 		if pkt != nil {
 			fmt.Println(pkt.String())
 		} else {
 			log.Printf("WARN: Dropped byte 0x%x\n", header)
-		}
-	}
-	if tmp != nil && !*keepTmp {
-		os.Remove(tmp.Name())
-	}
-}
-
-func parseEtfToEtm(id int, in *os.File, out *os.File) {
-	var err error
-	buf := make([]byte, 16)
-	var curId uint64 // Current ID in the trace (may not be ours...)
-	var compId uint64
-	var i uint
-
-	compId = uint64(id)
-	curId = 0
-
-	log.Debugf("compId: %v curId: %v", compId, curId)
-
-	for err == nil {
-		n, err := io.ReadAtLeast(in, buf, 16)
-
-		if err == io.ErrUnexpectedEOF || err == io.ErrShortBuffer {
-			fmt.Errorf("Error received reading input file: %q", err)
-		} else if n == 0 {
-			break
-		}
-
-		log.Debugf("Read %d bytes, current ID: %d", n, curId)
-
-		for i = 0; i < 14; i += 2 {
-			aux := buf[15] >> (i / 2) & 1
-			log.Debugf("buf[%0d]=%#v buf[%0d]=%#v buf[15]=%#v aux=%#v", i, buf[i], i+1, buf[i+1], buf[15], aux)
-			if buf[i]&1 == 0 { // Data mode
-				if curId == compId || *dbgDisIdCheck {
-					data := []byte{(buf[i] & 0xfe) | ((buf[15] >> (i / 2)) & 1)}
-					out.Write(data)
-					out.Write([]byte{buf[i+1]})
-					log.Debugf("Data mode, writing byte: %#v and %#v ID %d", data[0], buf[i+1], curId)
-				}
-			} else if buf[i]&1 == 1 && aux == 0 { // bit[0] set, new ID immediately
-				oldId := curId
-				byteId := (buf[i] >> 1) & 0x7f
-				curId, _ = binary.Uvarint([]byte{byteId})
-				log.Debugf("ID update %d -> %d", oldId, curId)
-				if curId == compId || *dbgDisIdCheck {
-					out.Write([]byte{buf[i+1]})
-					log.Debugf("New ID immediately, writing byte: %#v ID %d", buf[i+1], curId)
-				}
-			} else if buf[i]&1 == 1 && aux == 1 { // Update with new ID
-				oldId := curId
-				// Byte 1 corresponds to the old ID
-				if curId == compId || *dbgDisIdCheck {
-					out.Write([]byte{buf[i+1]})
-					log.Debugf("New ID, writing byte: %#v ID %d", buf[i+1], curId)
-				}
-				byteId := (buf[i] >> 1) & 0x7f
-				curId, _ = binary.Uvarint([]byte{byteId})
-				log.Debugf("ID update %d -> %d", oldId, curId)
-			} else {
-				log.Errorln("Not sure what to do with this byte[%d]=%#v byte[15]=%#v", i, buf[i], buf[15])
-			}
-		}
-		if buf[14]&1 == 1 { // Update ID
-			oldId := curId
-			byteId := (buf[14] >> 1) & 0x7f
-			curId, _ = binary.Uvarint([]byte{byteId})
-			log.Debugf("ID update %d -> %d", oldId, curId)
-		} else if curId == compId || *dbgDisIdCheck {
-			data := []byte{(buf[14] & 0xfe) | (buf[15]>>7)&1}
-			log.Debugf("Writing byte 14: %#v ID %d", data[0], curId)
-			out.Write(data)
 		}
 	}
 }
